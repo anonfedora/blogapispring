f1830fef0c9f4db711ff9204e060e97836d5655d
Musa Eleazar <musaeleazar090@gmail.com>
2024-08-02 03:09:41 +0100

    first commit

---
+++ src/main/java/com/anonfedora/blogapispring/BlogapispringApplication.java
@@ -0,0 +1,13 @@
+package com.anonfedora.blogapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogapispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogapispringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anonfedora/blogapispring/BlogapispringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonfedora.blogapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogapispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
